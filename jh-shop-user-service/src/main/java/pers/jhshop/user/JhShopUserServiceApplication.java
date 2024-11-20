038e50b0603ecaa326d4978ea3b3f7a0e5a7cacc
chenjiahao <834713285@qq.com>
2024-11-20 22:14:46 +0800

    init

---
+++ jh-shop-user-service/src/main/java/pers/jhshop/user/JhShopUserServiceApplication.java
@@ -0,0 +1,13 @@
+package pers.jhshop.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhShopUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhShopUserServiceApplication.class, args);
+    }
+
+}
