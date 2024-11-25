f4a3cf981da1a8edba9bf8c69f378291c58217a2
chenjiahao <834713285@qq.com>
2024-11-25 23:02:04 +0800

    init

--- jh-shop-user-service/src/main/java/pers/jhshop/user/JhShopUserServiceApplication.java
+++ jh-shop-user-service/src/main/java/pers/jhshop/user/JhShopUserServiceApplication.java
@@ -1,8 +1,12 @@
 package pers.jhshop.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
+@MapperScan(basePackages = "pers.jhshop.user.mapper")
 @SpringBootApplication
 public class JhShopUserServiceApplication {
 
